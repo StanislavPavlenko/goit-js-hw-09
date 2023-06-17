0619598b4bea29427a44b241ba163f0b4d4829af
StanislavPavlenko <voltfrag@gmail.com>
2023-06-17 13:14:40 +0300

    color swicher

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
 
+
+const refs = {
+    btnStart: document.querySelector('[data-start]'),
+    btnStop: document.querySelector('[data-stop]'),
+
+};
+let timerId = null;
+
+refs.btnStart.addEventListener('click', onStart);
+refs.btnStop.addEventListener('click', onStop);
+
+function btnStart() {
+    refs.btnStop.disabled = '';
+    refs.btnStart.disabled = 'disabled';
+    document.body.style.backgroundColor = getRandomHexColor();
+    timerId = setInterval(() => {
+        document.body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+}
+function onStop() {
+    refs.btnStart.disabled = '';
+    clearInterval(timerId);
+}
