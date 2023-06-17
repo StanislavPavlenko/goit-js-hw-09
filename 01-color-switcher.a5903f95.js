1c81f7544b7414dbf12880e0d1bbcd0206a441d4
StanislavPavlenko <121876138+StanislavPavlenko@users.noreply.github.com>
2023-06-17 10:15:02 +0000

    Deploying to gh-pages from @ StanislavPavlenko/goit-js-hw-09@0619598b4bea29427a44b241ba163f0b4d4829af 🚀

---
+++ 01-color-switcher.a5903f95.js
@@ -0,0 +1,2 @@
+!function(){var t={btnStart:document.querySelector("[data-start]"),btnStop:document.querySelector("[data-stop]")},n=null;t.btnStart.addEventListener("click",onStart),t.btnStop.addEventListener("click",(function(){t.btnStart.disabled="",clearInterval(n)}))}();
+//# sourceMappingURL=01-color-switcher.a5903f95.js.map
---
+++ 01-color-switcher.a645aaa0.js
@@ -0,0 +1,2 @@
+const t={btnStart:document.querySelector("[data-start]"),btnStop:document.querySelector("[data-stop]")};let e=null;t.btnStart.addEventListener("click",onStart),t.btnStop.addEventListener("click",(function(){t.btnStart.disabled="",clearInterval(e)}));
+//# sourceMappingURL=01-color-switcher.a645aaa0.js.map
