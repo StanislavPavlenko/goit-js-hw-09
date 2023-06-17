073f1a76370ceb4616802f9ee7c6e5daf775bc60
StanislavPavlenko <voltfrag@gmail.com>
2023-06-18 01:21:02 +0300

    123

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,33 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+document.querySelector('.form').addEventListener('submit', submitBtn);
+
+function submitBtn(e) {
+  e.preventDefault();
+  const inputDelay = Number(document.querySelector('input[name="delay"]').value);
+  const inputStep = Number(document.querySelector('input[name="step"]').value);
+  const inputAmount = Number(document.querySelector('input[name="amount"]').value);
+
+for (let i = 0; i < inputAmount; i++) {
+    createPromise(i + 1 , inputDelay + i * inputStep)
+      .then(({ position, delay }) => {
+        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+  }
+}
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
